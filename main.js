82f4a717f9e418a8b6b5cfb3c9eb52fa1d4e3eee
Simon Kallkvist <simon.kallkvist@gmail.com>
2024-04-09 15:43:41 +0200

    Added method for comparison

--- main.js
+++ main.js
@@ -21,8 +21,40 @@ class Pokemon {
   }
 
   //   Compare stats and color the "winner" between two pokemon
-  static ComparePokemon(pokemon1, pokemon2) {
-    console.log(pokemon1, pokemon2);
+  static ComparePokemon() {
+    pokeHolder.forEach((pokemon) => {
+      console.log(pokemon);
+    });
+
+    // getting all the stats
+    let stats = [];
+    pokeHolder.forEach((pokemon) => {
+      console.log(pokemon.stats);
+      stats.push(pokemon.stats);
+    });
+
+    // filtering all thge stats
+    let filterdStats = [];
+    for (i = 0; i < stats[0].length; i++) {
+      if (stats[0][i].base_stat > stats[1][i].base_stat) {
+        filterdStats.push(stats[0][i].base_stat);
+      } else if (stats[0][i].base_stat < stats[1][i].base_stat) {
+        filterdStats.push(stats[1][i].base_stat);
+      }
+    }
+
+    // nesting loops too find matching values, and adding a class to the matches
+    let statDivs = document.querySelectorAll('.stats');
+    console.log(statDivs);
+    statDivs.forEach((div) => {
+      div.childNodes.forEach((node) => {
+        filterdStats.forEach((stat) => {
+          if (node.innerHTML.includes(stat)) {
+            node.classList.add('winner');
+          }
+        });
+      });
+    });
   }
 }
 
@@ -54,10 +86,12 @@ let renderSelect = async (pokeListPromise) => {
 
 // Get the specific pokemon and send its info to an instance inside Pokemon class
 let gatherPokeInfo = async (pokemonURL) => {
-  let pokemon = await getData(pokemonURL);
+  if (pokemonURL) {
+    let pokemon = await getData(pokemonURL);
 
-  //   Create an instance from api Call
-  createPokeInstance(pokemon);
+    //   Create an instance from api Call
+    createPokeInstance(pokemon);
+  }
 
   //   Print to the dom
   if (pokeHolder.length === 2) {
@@ -66,6 +100,7 @@ let gatherPokeInfo = async (pokemonURL) => {
   pokeHolder.forEach((pokemon) => {
     renderPokemon(pokemon);
   });
+  addCompareBtn();
 };
 
 // listen for change on the pokemon list, get the url associated and send to gatherPokeInfo()
@@ -113,6 +148,21 @@ let renderPokemon = (pokemon) => {
   let card = document.createElement('div');
   card.classList.add('card');
 
+  console.log(pokemon.types[0].type.name);
+  if (pokemon.types[0].type.name == 'water') {
+    card.classList.add('water');
+  } else if (pokemon.types[0].type.name == 'fire') {
+    card.classList.add('fire');
+  } else if (pokemon.types[0].type.name == 'grass') {
+    card.classList.add('grass');
+  } else if (pokemon.types[0].type.name == 'electric') {
+    card.classList.add('electric');
+  } else if (pokemon.types[0].type.name == 'normal') {
+    card.classList.add('normal');
+  } else {
+    card.classList.add('generic');
+  }
+
   //   adding the top info
   let info = document.createElement('div');
   info.classList.add('info');
@@ -139,7 +189,7 @@ let renderPokemon = (pokemon) => {
   let attacks = document.createElement('div');
   attacks.classList.add('attacks');
 
-  //   setting a fixed loop to two so that only two attacks are showing on the card
+  // setting a fixed loop to two so that only two attacks are showing on the card
   for (i = 0; i < 2; i++) {
     if (pokemon.moves[i]) {
       let attack = document.createElement('div');
@@ -169,6 +219,7 @@ let renderPokemon = (pokemon) => {
   let stats = document.createElement('div');
   stats.classList.add('stats');
 
+  //   Cycling the stats and rendering to the DOM
   pokemon.stats.forEach((stat) => {
     let label = document.createElement('label');
     label.setAttribute('for', `${stat.stat.name}`);
@@ -180,10 +231,54 @@ let renderPokemon = (pokemon) => {
     stats.append(label, progress);
   });
 
+  //   Adding a deletebtn to the cards
+  let deleteBtn = document.createElement('button');
+  deleteBtn.classList.add('deleteBtn');
+  deleteBtn.innerText = 'X';
+
+  deleteBtn.addEventListener('click', () => {
+    deleteCard(pokemon);
+  });
+
+  card.append(deleteBtn);
   pokeCard.append(stats);
 
   pokemonHolder.append(pokeCard);
   pokemonContainer.append(pokemonHolder);
+};
+
+// Delete function, finding the index of the pokemon and removing from the list, clearing the DOM and re-rendering
+let deleteCard = (pokemon) => {
+  console.log(pokeHolder);
+  pokeHolder.splice(pokeHolder.indexOf(pokemon), 1);
+  console.log(pokeHolder);
+  clearTheDOM();
+  addCompareBtn();
+  gatherPokeInfo();
+};
+
+let addCompareBtn = () => {
+  let compareBtn = document.querySelector('.compareBtn');
+
+  if (pokeHolder.length === 2) {
+    if (!compareBtn) {
+      compareBtn = document.createElement('button');
+      compareBtn.classList.add('btn', 'compareBtn');
+      compareBtn.innerText = 'Compare';
+      compareBtn.addEventListener('click', () => {
+        Pokemon.ComparePokemon();
+      });
+      container.append(compareBtn);
+    }
+  } else {
+    if (compareBtn) {
+      compareBtn.remove();
+    }
+  }
+};
+
+let clearTheDOM = () => {
+  pokemonContainer.innerHTML = '';
 };
 
 // Initial api call to get all options for the select
